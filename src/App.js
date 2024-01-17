71c2c72d654d1c86018b1d01c65be108b1fa6bed
ArtemH <artem.xolod.90@gmail.com>
2024-01-17 16:48:32 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
 import {useEffect} from "react";
-const tg = window.Telegram.WebApp;
+
 
 function App() {
 
-    useEffect(() => {
-       tg.ready();
-    }, []);
-
-    const onClose = () => {
-
-    }
-
   return (
     <div className="App">
-work
-        <button onClick={onClose}>Закрыть</button>
+        work
+
     </div>
   );
 }
