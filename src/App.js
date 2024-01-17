0bd1c0564cbc70eea5915eaa6caee212c6e3caa3
ArtemH <artem.xolod.90@gmail.com>
2024-01-17 16:58:16 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import {useEffect} from "react";
-
-
+const tg = window.Telegram.WebApp;
 function App() {
+
+    useEffect(() => {
+       tg.ready();
+    }, []);
+
+
 
   return (
     <div className="App">
-        work
+work
 
     </div>
   );
