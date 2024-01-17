9bba2054d98cca673b120d3fc36c307bff63a275
ArtemH <artem.xolod.90@gmail.com>
2024-01-17 17:25:39 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
 
 
 function App() {
@@ -14,8 +15,8 @@ function App() {
 
   return (
     <div className="App">
-work
-    <button onClick={onToggleButton}>toggle</button>
+        <Header/>
+        <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
